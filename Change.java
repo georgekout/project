49e208d8dc62cd942bacbf4bcea8b300f14042c1
Polymnia Benardou <polymnia_benardou@yahoo.com>
2018-12-29 18:19:33 +0200

    Add files via upload

---
+++ Change.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Change{
+
+    public static void ChanEl(List<List<Object>>outerList){
+		System.out.printf("\n Press 1, in order to change column name. \n Press 2, in order to change an element.");
+		Scanner answer = new Scanner(System.in);
+		int whatTochange = answer.nextInt();
+
+		if(whatTochange == 1) {
+			System.out.printf("\n Give the name of the column that you want to change:");
+			Scanner col = new Scanner(System.in);
+			Object column = col.next();
+			int found = Change.searchColumn(outerList,column);
+
+			if(found != -1){
+				System.out.printf("\n Insert new name for column:");
+				Scanner newName = new Scanner(System.in);
+				Object nColName = newName.next();
+				outerList.get(0).set(found,nColName);
+			} else {
+				System.out.printf("\n The Column you want to change does not exist. \n");
+			}
+
+		} else if(whatTochange == 2) {
+			System.out.printf("\n Please insert the name of the column that contains the element you want to change:");
+			Scanner col = new Scanner(System.in);
+			Object column = col.next();
+			int columnFound = Change.searchColumn(outerList,column);
+
+			if (columnFound != -1){
+				System.out.printf("\n Please insert the element you want to change:");
+				Scanner el = new Scanner(System.in);
+				Object element = el.next();
+				int searchEl = Change.searchElement(outerList, element, columnFound);
+
+				if(searchEl != -1){
+					System.out.printf("\n");
+					System.out.printf("Please insert new Object:");
+					Scanner newEl = new Scanner(System.in);
+					Object newElement = newEl.next();
+					outerList.get(searchEl).set(columnFound,newElement);
+
+
+				} else {
+					System.out.printf("\n The element you inserted does not exist in the database or in the column you inserted previously. Please try again.\n");
+				}
+
+			}else {
+				System.out.printf("\n The column name you inserted does not exist in the Database.\n");
+			}
+		}else {
+			System.out.println("\n The option you selected does not exist. Please try again. \n");
+		}
+}
+
+public static int searchColumn(List<List<Object>>outerList,Object Elsearched){
+	int i = 0;
+	Boolean flag = false;
+
+	do{
+			if(outerList.get(0).get(i).equals(Elsearched) ){
+				flag = true;
+			} else{
+				i++;
+			}
+
+	}while(i < outerList.size() && flag == false);
+
+	if(flag == false){
+		i = -1;
+	}
+	return i;
+}
+
+public static int searchElement(List<List<Object>>outerList,Object Elsearched, int col){
+	int i = 0;
+	Boolean flag = false;
+	int elFound = -1;
+
+	do{
+		if(outerList.get(i).get(col).equals(Elsearched)){
+			elFound = i;
+			flag = true;
+		} else {
+			i++;
+		}
+		}while(flag == false && i < outerList.size());
+
+		return elFound;
+}
+
+}
--- Menu.java
+++ Menu.java
@@ -1,7 +1,13 @@
-public class Menu {
+public class Menu{
 
-    public static void main(String[] args) {
-        Input input = new Input();
-        input.categoryInput();
-    }
+	public static void main(String[] args){
+		Input input = new Input();
+		input.categoryInput();
+		Change change = new Change();
+		Change.ChanEl(input.outerList);
+
+		 for (int i = 0; i < input.outerList.size() ;i++) {
+		            System.out.println(input.outerList.get(i));
+        }
+	}
 }
